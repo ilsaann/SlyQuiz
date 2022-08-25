14504bbcb225c255030af77fdce83b733bde5786
Ilsa <hamptonilsa96@gmail.com>
2022-08-25 18:54:58 -0400

    fixed duplicate sly

--- sly-quiz-react/src/About.js
+++ sly-quiz-react/src/About.js
@@ -10,23 +10,6 @@ export default function About() {
       <button type="button" className="collapsible" id="sly">
         Sly
       </button>
-      <div className="content" id="sly">
-        <img src={sly} alt="pic of sly" />
-        <p>
-          Sly is a thief raccoon who is descended from a long line of master
-          thieves coming from medieval Europe, feudal Japan, and the American
-          Western frontier, tracing all the way back to Ancient Egypt as well as
-          pirates.
-          <br />
-          When Sly was eight years old, his father was killed by a gang called
-          the Fiendish Five. The gang stole the "Thievius Raccoonus," a book
-          containing the Cooper family's thieving techniques and moves.
-          <br />
-          Sly was sent to an orphanage. There he met Bentley, a genius turtle,
-          and Murray, a comically self-confident and brawny hippopotamus.
-        </p>
-      </div>
-      {/* <Details /> */}
       <div className="content">
         <img src={sly} alt="pic of sly" />
         <p>
