06fbce5badafe578077644b6ffcb1ed9eede27da
Ilsa <hamptonilsa96@gmail.com>
2022-08-25 18:53:36 -0400

    got toggle, lost toggle, now we cry

--- sly-quiz-react/src/App.js
+++ sly-quiz-react/src/App.js
@@ -1,8 +1,20 @@
 import Quiz from "./Quiz";
 import Result from "./Result";
 import About from "./About";
+//import React, { useState, useEffect } from "react";
 
 function App() {
+  // const [display, setDisplay] = useState("none");
+  // function handleCollapse() {
+  //   if (this.display === "none") {
+  //     setDisplay("block");
+  //   } else {
+  //     setDisplay("none");
+  //   }
+  // }
+  // useEffect(() => {
+  //   document.getElementsByClassName(".content").style = { display };
+  // });
   return (
     <>
       <div className="about">
@@ -19,3 +31,18 @@ function App() {
 }
 
 export default App;
+
+// const display = useState([]);
+// original way to make a collapsible portion- however we don't want to work directly with the DOM gotta get the virtual DOM
+// var coll = document.getElementsByClassName("collapsible");
+// for (var i = 0; i < coll.length; i++) {
+// coll[i].addEventListener("click", function () {
+// coll.classList.toggle("active");
+// var content = this.nextElementSibling;
+// if (content.style.display === "block") {
+//   content.style.display = "none";
+// } else {
+//   content.style.display = "block";
+// }
+//  });
+//  }
---
+++ sly-quiz-react/src/Details.js
@@ -0,0 +1,12 @@
+// import React from "react";
+// import sly from "./images/sly.png";
+// import murray from "./images/murray.png";
+// import bentley from "./images/bentley.png";
+
+// export default function Details() {
+//  const slyAbout =
+
+//   return (
+
+//   );
+// }
