4517b1295a02924431bff93317a338c31f24ce4c
Ilsa <hamptonilsa96@gmail.com>
2022-08-26 23:49:41 -0400

    trying to figure out my requests

--- sly-quiz-react/src/Quiz.js
+++ sly-quiz-react/src/Quiz.js
@@ -1,55 +1,90 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 //this file will handle Displaying the quiz questions and answers
+
 export default function Quiz() {
-  // const [playing, setPlaying] = useState(false);
+  const currentQuestion = {};
+  const answerChoices = [];
+  const playerAnswer = { S: 0, M: 0, B: 0 };
+  const number = [1];
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    getQuestion(number);
+    populateQA();
+  }, []);
+
+  function Play() {
+    console.log("i was clicked in play", currentQuestion, playing);
+
+    if (playing && currentQuestion.length !== 0) {
+      return (
+        <>
+          <div className="question-card">
+            <h2>{currentQuestion.question}</h2>
+            <ul>
+              <li>{answerChoices[0]}</li>
+              <li>{answerChoices[1]}</li>
+              <li>{answerChoices[2]}</li>
+            </ul>
+          </div>
+        </>
+      );
+
+      //quiz blank card
+    } else {
+      return <div>oops</div>;
+    }
+  }
+  //should useEffect here so that it reloads with each new question
+
+  function getQuestion(num) {
+    fetch("/questions").then((res) => {
+      const questions = res.body;
+      console.log("getQuestion", questions);
+      if (num > 0 && num < questions.length) {
+        currentQuestion.question = questions[num];
+      }
+      //make sure that the question is an object of the questionid and question
+      //take json.object and set currentQuestion = questions[0]
+    });
+  }
+
+  // now the correlating answerchoices need to be fetched
+  function populateQA() {
+    //get the answers by question ID
+    fetch(`/answers/${number}`).then((res) => {
+      const answers = res.body;
+      console.log("populateQA", answers);
+
+      for (const e in answers) {
+        answerChoices.push(e);
+      }
+    });
+
+    return answerChoices;
+  }
+
+  // useEffect(() => {
+  //   getQuestion();
+  //   populateQA();
+  // }, []);
+
+  //maybe a useRef for the button/card
+  //maybe look into that axios library
   return (
     <>
-      <div>Let's take the character quiz</div>
-      {/* <button className="play" onClick={setPlaying(!playing)}>
-        Take Character Quiz
-      </button> */}
+      <button
+        className="play"
+        onClick={() => {
+          setPlaying(!playing);
+          Play();
+        }}
+      >
+        TAKE CHARACTER QUIZ
+      </button>
+
       {/* playing && <div>{Question()} */}
     </>
   );
 }
-
-function Question(questionid) {
-  //function getQuestion(){//
-  // fetch("/questions", (questions) => {
-  //console.log(questions); make sure that the question is an object of the questionid and question
-  //take json.object and set currentQuestion = questions[0]
-  //});
-  //}
-
-  //   function populateQA() {
-  //get the answers by question ID
-  //         fetch("/answer", (answers) => {
-  //         console.log(answers);
-  //         });
-  //     }
-
-  //currentQuestion will be an object of the question and the question id
-  const currentQuestion = {
-    questionid,
-    question: "Do you prefer sneaky or range attacks?",
-  };
-  //array of answer objects, answer object will include answercode, answer, questionid
-  const answers = [
-    { answercode: "M", answer: "Fists, Actually" },
-    { answercode: "B", answer: "Range" },
-    { answercode: "S", answer: "Sneaky" },
-  ];
-
-  return (
-    <>
-      <div className="question-card">
-        <h2>{currentQuestion.question}</h2>
-        <ul>
-          <li className="sly">{answers[0].answer}</li>
-          <li className="bentley">{answers[1].answer}</li>
-          <li className="murray">{answers[2].answer}</li>
-        </ul>
-      </div>
-    </>
-  );
-}
+//onClick={}
